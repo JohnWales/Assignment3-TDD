01a2c36a32eb63d78e5baf8b63a9320d355bf94d
walebot <walesjj@gmail.com>
2020-12-15 21:40:33 +0000

    Colour class Constructor created
    First I must create a constructor for Colour so I can create the first Unit Test

--- src/main/java/cs3318/verify/Colour.java
+++ src/main/java/cs3318/verify/Colour.java
@@ -2,4 +2,29 @@ package cs3318.verify;
 
 public class Colour {
 
+    /**
+     * Initialising my RGB variables
+     */
+    int red;
+    int green;
+    int blue;
+
+    /**
+     * Creating a max and min value for the RGB model
+     */
+    final static int MAX_VALUE = 255;
+    final static int MIN_VALUE = 0;
+
+    /**
+     * First Constructor for this class
+     * Takes in integer values for RGB colour
+     * @param r represents Red in the RGB value
+     * @param g represents Green in the RGB value
+     * @param b represents Blue in the RGB value
+     */
+    public Colour(int r, int g, int b) {
+        red = r;
+        green = g;
+        blue = b;
+    }
 }
