8c419b710ee5b23e28519c923681960a864fab71
walebot <walesjj@gmail.com>
2020-12-15 21:45:44 +0000

    Constructor verified
    created a testColourNotNull() method

--- src/test/java/cs3318/verify/ColourTest.java
+++ src/test/java/cs3318/verify/ColourTest.java
@@ -1,7 +1,28 @@
 package cs3318.verify;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ColourTest {
 
+    /**
+     * This is where colour objects are created
+     */
+    Colour colourValid1;
+
+
+    /**
+     * Creating a colour that satisfies all RGB values
+     */
+    @Test
+    public void testColourNotNull() {
+        try {
+            this.colourValid1 = new Colour(100, 100, 50);
+        }
+        catch(Exception e) {
+
+        }
+        assertNotNull(this.colourValid1);
+    }
 }
